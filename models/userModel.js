57b7edb4c0c41b90c6aa012a19462ff8f8076032
Marcelo Taborda <86986422+Marcelo-Taborda@users.noreply.github.com>
2023-06-03 16:23:09 -0300

    Add files via upload

---
+++ models/userModel.js
@@ -0,0 +1,15 @@
+// auth/models/userModel.js
+
+const mongoose = require('mongoose');
+
+// Definir el esquema del usuario
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+// Crear el modelo de usuario
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;

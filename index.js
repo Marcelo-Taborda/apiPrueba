5fe51d8c7bf2538d29404498058461ded59625ad
Marcelo Taborda <86986422+Marcelo-Taborda@users.noreply.github.com>
2023-06-09 00:18:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser('your-jwt-secret'));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://tu-sitio-web.com'); // Reemplaza con el origen permitido para las solicitudes (puede ser '*' para permitir cualquier origen)
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Reemplaza con el origen permitido para las solicitudes (puede ser '*' para permitir cualquier origen)
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Reemplaza con los métodos HTTP permitidos
